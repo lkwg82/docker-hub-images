0ee787772d509b58464fc0ccc500881846b96fa8
Lars K.W. Gohlke <lkwg82@gmx.de>
2017-01-08 17:38:11 +0100

    upgraded tests

--- combined_build_images/jenkins_with_docker/Readme.md
+++ combined_build_images/jenkins_with_docker/Readme.md
@@ -4,4 +4,4 @@ Have a jenkins instance with docker inside
 docker run -ti -v /var/run/docker.sock:/var/run/docker.sock lkwg82/jenkins_with_docker
 ```
 
-Hint: currently it is packaged with docker 1.9
+Hint: see docker version in Dockerfile
--- combined_build_images/jenkins_with_docker/test.sh
+++ combined_build_images/jenkins_with_docker/test.sh
@@ -1,19 +1,26 @@
 #! /bin/bash
 
-set -ex
+set -e
 
 docker build -t test .
-cid=$(docker run --network=host -d test)
+cid=$(docker run --network=host -v /var/run/docker.sock:/var/run/docker.sock -d test)
 
 function finish {
 	docker rm -f ${cid}  
 }
 trap finish EXIT
 
+
+# tests
+echo "testing"
+docker exec ${cid} docker --version > /dev/null
+docker exec ${cid} docker run alpine echo 'Hello world' > /dev/null
+docker exec ${cid} docker-compose --version > /dev/null
+
 sleep 10
 for i in `seq 1 10`; do
-	echo "try $i"
-	[ "$(docker inspect --format '{{.State.Health.Status}}' $cid)" == "healthy" ] && exit 0
+	echo " checking status $i "
+	[ "$(docker inspect --format '{{.State.Health.Status}}' $cid)" == "healthy" ] && echo "... success " && exit 0
 	sleep 5
 done	
 
