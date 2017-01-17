f8e3ec74ab9d3de7fb8044da3abe675f5550ad8f
Lars K.W. Gohlke <lkwg82@gmx.de>
2017-01-17 01:02:25 +0100

    adds update check

--- selenium/alpine/release.sh
+++ selenium/alpine/release.sh
@@ -18,5 +18,19 @@ chromiumVersion=$(dr chromium-browser --version | cut -d\  -f2 | tr -d '\r')
 alpineVersion=$(dr cat /etc/alpine-release | tr -d '\r') 
 
 release="alpine${alpineVersion}_jre8u${jreVersion}_chromium${chromiumVersion}"
-docker tag $image lkwg82/selenium:$release
-docker push lkwg82/selenium:$release
+
+function tagPushDel { 
+	docker tag $image $1; 
+	docker push $1; 
+	docker rmi $1; 
+}
+
+docker pull lkwg82/selenium:latest
+latest=$(docker run --rm -t lkwg82/selenium:latest sha1sum /lib/apk/db/installed | cut -d\  -f1)
+current=$(docker run --rm -t $image sha1sum /lib/apk/db/installed | cut -d\  -f1)
+
+if [ "$latest" != "$current" ]; then
+	tagPushDel lkwg82/selenium:$release
+	tagPushDel lkwg82/selenium:latest
+fi
+
