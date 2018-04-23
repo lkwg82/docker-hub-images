bd0af59ffc5a945e159a9c034308ffad4d141cfb
Lars K.W. Gohlke <lkwg82@gmx.de>
2018-04-23 22:37:05 +0200

    add avi-files to list of movie converter

--- movie_converter/convertCamVideo2ArchivVideo.sh
+++ movie_converter/convertCamVideo2ArchivVideo.sh
@@ -51,5 +51,5 @@ function _convert(){
 
 export -f _convert
 
-find -maxdepth 1 -type f -name "*MOV" -o -name "*3gp" -o -name "*mp4" | grep -v ".out.mp4" | xargs -n1 -I {} bash -c '_convert {}'
+find -maxdepth 1 -type f -name "*MOV" -o -name "*3gp" -o -name "*mp4" -o -name "*.avi" | grep -v ".out.mp4" | xargs -n1 -I {} bash -c '_convert {}'
 #_convert $1
