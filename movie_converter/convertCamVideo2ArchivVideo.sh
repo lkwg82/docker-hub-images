446824c40d373655ce7a7c3094a3ad7b6645bafe
Lars K.W. Gohlke <lkwg82@gmx.de>
2018-04-23 21:37:56 +0200

    adds movie converter

---
+++ movie_converter/convertCamVideo2ArchivVideo.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+set -e
+set -x
+
+size=-1
+
+function exitEnter { echo $1; echo "press ENTER to exit"; read; }
+function checkInstalled {
+	printf "checking %-30s installed : "  $1;
+	type $1 >/dev/null 2>&1 && echo "ok" || { echo "fail"; exitEnter; }
+}
+
+checkInstalled exiftool
+
+function determineMaxVideoSize(){
+	local source=$1
+	local width=$(exiftool $source | grep -i "^Image Width" | sed -e 's#.*: ##');
+	local height=$(exiftool $source | grep -i "^Image Height" | sed -e 's#.*: ##');
+	
+	local max=$(($width>$height ? $width : $height));
+	
+	if [ $max -gt 1280 ];
+	then
+		size=hd720;
+	else
+		size=$width"x"$height;
+	fi
+}
+
+function _convert(){
+	local source=$1
+	local target="$1.out.mp4"
+	local targetTmp="$1.tmp.out.mp4"
+	if [ -f $target ];
+	then
+		echo "skipping '$source', because '$target' already exists"
+	else
+			
+		#~ local metadata="$1.avi.metadata"
+		#~ avconv -y -i $source -f ffmetadata -map_metadata 0:g $metadata
+							
+		#~ local audioOpts="-acodec libmp3lame -ab 160000  `# mp3 160kB/s`"
+		# https://trac.ffmpeg.org/wiki/Encode/MP3
+		local audioOpts="-acodec libmp3lame -q:a 4  `# mp3 140-185kbit/s`"
+		#~ local audioOpts="-c:a copy"
+		#~ local audioOpts=""
+		
+		
+#		local videoOpts="-vcodec libx264 -maxrate 4000k -bufsize 1M"
+		local videoOpts="-vcodec libx265 -maxrate 4000k -bufsize 1M -preset veryslow -threads 1"
+		#~ local videoOpts="-c:v copy"
+		#~ local videoOpts="-codec copy"
+		
+
+		local cmd="ffmpeg -i $source \
+			$audioOpts \
+			$videoOpts \
+			$targetTmp"
+
+		$cmd && \
+			mv $targetTmp $target && \
+			touch -r "$source" "$target" 
+	fi
+}
+
+export -f _convert
+export -f determineMaxVideoSize
+
+find -maxdepth 1 -type f -name "*MOV" -o -name "*3gp" -o -name "*mp4" | grep -v ".out.mp4" | xargs -n1 -I {} bash -c '_convert {}'
+#_convert $1
---
+++ movie_converter/test/simple/test.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+set -e
+
+
+function finish {
+	local exitCode=$?
+    set +x
+
+	echo "cleanup"
+
+	echo "---------"
+	rm -f ${testFile}
+	if [ "$exitCode" == "0" ]; then
+		echo "Test: SUCCESS"
+	else
+		echo "Test: failed"
+		exit ${exitCode}
+	fi
+}
+trap finish EXIT
+
+# build image
+pushd ../..
+echo -n "building image ... "
+iidFile=$(tempfile)
+docker build --iidfile ${iidFile} .  > /dev/null
+echo ok
+popd
+
+
+cmd="docker run --rm -ti \
+	--cap-drop ALL \
+	--volume ${PWD}:/data \
+	--user ${UID} \
+	$(cat ${iidFile})"
+
+
+#set -x
+
+testFile="VID_20180423_205710.mp4.out.mp4";
+rm -f ${testFile}
+
+# tests
+$cmd ffmpeg -version >/dev/null
+
+echo "test convert run"
+$cmd convertCamVideo2ArchivVideo > /dev/null
+
+echo "test audio"
+$cmd ffprobe ${testFile} | grep "Audio: mp3 (mp4a / 0x6134706D), 48000 Hz, stereo, s16p, 92 kb/s" > /dev/null
+
+echo "test video"
+$cmd ffprobe ${testFile} | grep "Video: hevc (Main) (hev1 / 0x31766568), yuv420p(tv, progressive), 240x320" > /dev/null
+
+
+
