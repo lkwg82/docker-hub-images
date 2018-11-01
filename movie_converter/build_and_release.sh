9942e44d141b3f28d6516ce6583864ed0e165f27
Lars K.W. Gohlke <lkwg82@gmx.de>
2018-11-01 12:09:45 +0100

    prevents building release from dirty worktree

--- movie_converter/build_and_release.sh
+++ movie_converter/build_and_release.sh
@@ -2,5 +2,10 @@
 
 set -o pipefail
 
+if [[ $(git diff --stat) != '' ]]; then
+  echo 'dirty repo, plz commit or revert'
+  exit 1
+fi
+
 docker build -t lkwg82/movie-converter .
 docker push lkwg82/movie-converter
